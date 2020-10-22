c56d20a9a1f964654b102a6c6c39e1d7832632c5
Sean Morgan <insideseanshead@gmail.com>
2020-10-22 13:43:44 -0700

    updated server.js

--- controllers/html-controllers.js
+++ controllers/html-controllers.js
@@ -3,5 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render("index", {})
+    res.render("all", {})
 })
+
+router.get("")
