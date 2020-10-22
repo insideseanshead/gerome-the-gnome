df6414902d27f9aef1a969ed28726518a47f0d05
David Guthmann <drguthmann@cox.net>
2020-10-21 22:47:27 -0500

    added tested form for creating a character

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,3 +2,25 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+console.log("safe for work comment");
+
+$("#newChar").on("submit",event=>{
+    event.preventDefault();
+    console.log('new character made!');
+    const charObj = {
+        gender: $("#gender").val(),
+        race: $("#race").val(),
+        class: $("#class").val(),
+        mainatr: $("#mainatr").val()
+    }
+    console.log(charObj)
+    // $.ajax({
+    //     method:"POST",
+    //     url:"/api/character",
+    //     data:charObj
+    // }).then(data=>{
+    //     console.log(data);
+    //     window.location.href= "/loadthispage"
+    // })
+})
+
