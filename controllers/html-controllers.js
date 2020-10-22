ac5784cd0d4a1d793c3e6eb992c25ed1550ab0b8
Jose Morales <jmorales072095@gmail.com>
2020-10-22 14:16:49 -0700

    added basic rendering routes

--- controllers/html-controllers.js
+++ controllers/html-controllers.js
@@ -3,7 +3,28 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-    res.render("all", {})
+    res.render("create", {})
 })
 
-router.get("")
+router.get("/create", function(req, res) {
+    res.render("create", {})
+})
+
+router.get("/all", function(req, res) {
+    db.Character.findAll({}).then(character => {
+            const characterJson = character.map(charObj => {
+                return charObj.toJSON()
+            })
+            console.log(characterJson)
+        })
+        // res.render("all", {})
+    res.send("Hello its working")
+})
+
+router.get("/search", function(req, res) {
+    res.render("search", {})
+})
+
+router.get("/edit", function(req, res) {
+    res.render("edit", {})
+})
